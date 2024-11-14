24d20bf8feb917ede093bfeb165634612464f4a0
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-11-14 17:48:42 -0300

    add buttons download,share,like

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
 import screenBackground from "../../assets/background.png";
 import play from "../../assets/play.png";
+import save from "../../assets/save.png";
+import download from "../../assets/download.png";
+import share from "../../assets/share.png";
+import like from "../../assets/like.png";
 import Button from "../../components/Button";
 //import "../../Reset.css";
 import "./styles.css";
@@ -15,20 +19,49 @@ export default function Home() {
             alt="screenBackground"
           />
         </div>
-        <div className="mainInfo">
-          <h1 className="titleMovie">The Last Of Us Season 1</h1>
-          <p className="descriptionMovie">
-            9 Episodes • 2022 • Fantasy • Actions
-          </p>
-        </div>
-        <div className="buttonsMain">
-          {/* <button className="buttonActionMovie">
-            <img src={play} alt="" height={30} width={30} />
-            Ver Trailer
-          </button> */}
-          <Button icon={play} text="Ver Trailer" className="buttonActionMovie" />
-          <Button text="Adicionar na minha lista" className="buttonListMovie" variant="secondary" />
-          <Button text="Adicionar na minha lista" className="buttonListMovie" variant="danger" />
+        <div className="mainInfoGeneral">
+          <div className="mainInfo">
+            <Button text="Séries" className="buttonActionCategory" />
+            <h1 className="titleMovie">The Last Of Us Season 1</h1>
+            <p className="descriptionMovie">
+              9 Episodes • 2022 • Fantasy • Actions
+            </p>
+          </div>
+          <div className="buttonsWrapper">
+            <div className="buttonsMain">
+              <Button
+                icon={play}
+                text="Ver Trailer"
+                className="buttonActionMovie"
+              />
+              <Button
+                icon={save}
+                text="Adicionar na minha lista"
+                className="buttonListMovie"
+                variant="secondary"
+              />
+            </div>
+            <div className="buttonsAlternatives">
+              <Button
+                icon={download}
+                text="Download"
+                className="buttonListMovie"
+                variant="danger"
+              />
+              <Button
+                icon={share}
+                text="Share"
+                className="buttonListMovie"
+                variant="danger"
+              />
+              <Button
+                icon={like}
+                text="Like"
+                className="buttonListMovie"
+                variant="danger"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
