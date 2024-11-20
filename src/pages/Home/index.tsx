d63d2b5f3fdd7d109e27b57c92c174c4f4d4b2cf
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-11-20 18:10:22 -0300

    add header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,11 @@ import save from "../../assets/save.png";
 import download from "../../assets/download.png";
 import share from "../../assets/share.png";
 import like from "../../assets/like.png";
+import user from "../../assets/user.png";
+import search from "../../assets/search.png";
+import arrow from "../../assets/ArrowDown.png";
+import bell from "../../assets/bell.png";
+
 import Button from "../../components/Button";
 //import "../../Reset.css";
 import "./styles.css";
@@ -11,6 +16,35 @@ import "./styles.css";
 export default function Home() {
   return (
     <>
+      <div className="header">
+        <div className="headerLogo">
+          <h1 className="logo">
+            <span className="logo-icon">SS</span>
+            SaintStream
+          </h1>
+        </div>
+
+        <div className="headerNav">
+          <Button text="Home" className="buttonNav" />
+          <Button text="Discover" className="buttonNav" />
+          <Button text="Movie Release" className="buttonNav" />
+          <Button text="Forum" className="buttonNav" />
+          <Button text="About" className="buttonNav" />
+        </div>
+
+        <div className="headerControls">
+          <img src={search} alt="Search" className="searchIcon" />
+          <input type="text" className="search" placeholder="Search" />
+          <Button
+            text=""
+            icon="notification-icon"
+            className="buttonNotification"
+          />
+          <div className="headerUser">
+            <img src={user} alt="User" className="userAvatar" />
+          </div>
+        </div>
+      </div>
       <div className="main">
         <div className="mainScreen">
           <img
